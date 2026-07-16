10f4e9d608df6a476591407cfb5a53bb459f6b35
MeabedAbdo <me3bed@hotmail.com>
2026-07-16 18:45:57 +0300

    handle docker compose to build and up all containers

--- src/Services/Catalog/Catalog.API/Program.cs
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -43,8 +43,8 @@ builder.Services.AddMarten(options =>
     options.Connection(builder.Configuration.GetConnectionString("Database")!);
 }).UseLightweightSessions();
 
-if (builder.Environment.IsDevelopment())
-    builder.Services.InitializeMartenWith<CatalogInitialData>();
+//if (builder.Environment.IsDevelopment())
+//    builder.Services.InitializeMartenWith<CatalogInitialData>();
 
 builder.Services.AddExceptionHandler<CustomExceptionHandler>();
 
