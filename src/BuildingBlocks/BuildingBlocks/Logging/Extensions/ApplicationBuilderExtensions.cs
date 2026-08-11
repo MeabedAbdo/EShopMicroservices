8a63dffb48c388bdfeaf25cefcf3ce8febcf87dc
MeabedAbdo <me3bed@hotmail.com>
2026-08-11 19:11:35 +0300

    Apply logging in Building.Blocks shared proj

---
+++ src/BuildingBlocks/BuildingBlocks/Logging/Extensions/ApplicationBuilderExtensions.cs
@@ -0,0 +1,16 @@
+﻿using BuildingBlocks.Logging.Middleware;
+using Microsoft.AspNetCore.Builder;
+
+namespace BuildingBlocks.Logging.Extensions
+{
+    public static class ApplicationBuilderExtensions
+    {
+        public static IApplicationBuilder UseCompanyLogging(
+          this IApplicationBuilder app)
+        {
+            app.UseMiddleware<RequestLoggingMiddleware>();
+
+            return app;
+        }
+    }
+}
---
+++ src/BuildingBlocks/BuildingBlocks/Logging/Models/LoggingOptions.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BuildingBlocks.Logging.Models
+{
+    public class LoggingOptions
+    {
+        public string ApplicationName { get; set; } = string.Empty;
+
+        public bool EnableConsole { get; set; } = true;
+
+        public bool EnableFile { get; set; } = true;
+
+        public string FilePath { get; set; }
+            = "Logs/log-.txt";
+
+        public bool EnableSqlServer { get; set; }
+
+        public string? SqlConnectionString { get; set; }
+
+        public string TableName { get; set; }
+            = "Logs";
+        public bool EnableSeq { get; set; } = false;
+        public string? SeqURL { get; set; }
+    }
+}
